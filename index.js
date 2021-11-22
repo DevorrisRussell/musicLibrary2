847c2ac2b3861f898ad70a02d1cd455e3af0649a
Devorris Russell <devorris_r@yahoo.com>
2021-11-22 15:26:07 -0500

    added put file

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ const express = require('express');
 const repoContext = require('./repository/repository-wrapper');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.listen(5000, function() {
+    console.log("Server started. Listening on 5000");
+});
+
 app.get('/api/songs', (req, res) => { 
     const songs = repoContext.songs.findAllSongs();
     
     return res.send(songs);
 });
 
-
-app.listen(3000, function() {
-    console.log("Server started. Listening on 3000");
+app.get('/api/songs/:id', (req, res) => { 
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    
+    return res.send(song);
 });
+
+app.post('/api/songs', (req, res) => { 
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    
+    return res.send(addedSong);
+});
+
+app.get('/api/songs/:id', (req, res) => { 
+    const id = req.params.id;
+    const songPropertiesToUpdate = repoContext.songs.u(id);
+    
+    return res.send(song);
+});
+
+
